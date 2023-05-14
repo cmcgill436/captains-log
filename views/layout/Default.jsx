51569a6fed8485798d07832370d3c71f0f552005
cmcgill436 <candra.mcgill@gmail.com>
2023-05-14 18:14:40 -0500

    Added some styling

--- views/layout/Default.jsx
+++ views/layout/Default.jsx
@@ -1,15 +1,27 @@
 const React = require("react");
 const Nav = require("../components/Nav");
 
+const myStyle = {
+  container: {
+    backgroundImage:
+      "url(https://media.giphy.com/media/U3qYN8S0j3bpK/giphy.gif)",
+    color: "white",
+    textAlign: "center",
+    // a: {
+    //   color: "#84c7d7",
+    // },
+  },
+};
+
 class DefaultLayout extends React.Component {
   render() {
     return (
       <html>
         <head>
           <title>{this.props.title}</title>
-          <link rel="stylesheet" href="/css/app.css" />
+          <link rel="stylesheet" href="/app.css" />
         </head>
-        <body>
+        <body style={myStyle.container}>
           <Nav link={this.props.link} text={this.props.text} />
           <h1>{this.props.title}</h1>
           {this.props.children}
